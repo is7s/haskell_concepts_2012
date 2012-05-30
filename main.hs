f42e1adfc849341a03ffacc20d177aad2418bcf5
is7s <hussain.sob7y@gmail.com>
2012-05-30 18:26:38 +0300

    removed silly useless pattern match

--- main.hs
+++ main.hs
@@ -4,9 +4,9 @@ import Data.Map (Map,(!),member,insert,empty,toList)
 data Term = C String | V Char | S String [Term] deriving (Show,Eq)
 
 unify :: Term -> Term -> State (Map Char Term) Bool
-unify (V c) x = get >>= \env -> if member c env then unify (env!c) x
-        else modify (insert c x) >> return True
-unify p@_ v@(V _) = unify v p
+unify (V c) x = get >>= \env -> if member c env 
+    then unify (env!c) x else modify (insert c x) >> return True
+unify p v@(V _)      = unify v p
 unify (C x) (C y) | x == y = return True
 unify (S n t1) (S n' t2) | n == n' = go t1 t2 where
     go []     []     = return True
