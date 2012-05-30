279e94f936375b145e0cc6d0d571cc030d3d034f
is7s <hussain.sob7y@gmail.com>
2012-05-30 16:07:55 +0300

    Yet even more shortening

--- main.hs
+++ main.hs
@@ -1,19 +1,18 @@
 import Control.Monad.State
-import Data.Map (Map,(!),member,insert,empty)
+import Data.Map (Map,(!),member,insert,empty,toList)
 
 data Term = C String | V Char | S String [Term] deriving (Show,Eq)
 
-unify :: Term -> Term -> StateT (Map Char Term) Maybe [(Char,Term)]
-unify (V c) x = get >>= \env -> let term = env!c in
-    if member c env then unify term x >>= lift . Just . ((c,term):)
-        else modify (insert c x) >> lift (Just [(c,x)])
+unify :: Term -> Term -> State (Map Char Term) Bool
+unify (V c) x = get >>= \env -> if member c env then unify (env!c) x
+        else modify (insert c x) >> return True
 unify p@_ v@(V _) = unify v p
-unify (C x) (C y) | x == y = lift $ Just []
-unify (S n xs) (S n' ys) | n == n'   = zipWithM' unify xs ys >>= lift . Just . concat where
-    zipWithM' _ []     []     = return []
-    zipWithM' f (x:xs) (y:ys) = (:) `fmap` f x y `ap` zipWithM' f xs ys
-    zipWithM' _ _      _      = mzero
-unify _     _     = lift Nothing
+unify (C x) (C y) | x == y = return True
+unify (S n t1) (S n' t2) | n == n' = go t1 t2 where
+    go []     []     = return True
+    go (x:xs) (y:ys) = (&&) `fmap` unify x y `ap` go xs ys
+    go _      _      = return False
+unify _        _     = return False
 
 prolog :: Term -> Term -> Maybe [(Char,Term)]
-prolog t1 t2 = runStateT (unify t1 t2) empty >>= Just . fst
+prolog t1 t2 = let (a,s) = runState (unify t1 t2) empty in if a then Just (toList s) else Nothing
