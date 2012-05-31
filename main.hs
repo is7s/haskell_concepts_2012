2cbdfd73c42c4f81015d67a60f5d2d201b33d9bb
Hussain Sobhy <hussain.sob7y@gmail.com>
2012-05-31 09:24:51 +0200

    Operarotized unification and changed layout

--- main.hs
+++ main.hs
@@ -4,15 +4,15 @@ import Data.Map (Map,(!),member,insert,empty,toList)
 data Term = C String | V Char | S String [Term] deriving (Show,Eq)
 
 unify :: Term -> Term -> State (Map Char Term) Bool
-unify (V c) x = get >>= \env -> if member c env 
+unify (V c)    x                   = get >>= \env -> if member c env 
     then unify (env!c) x else modify (insert c x) >> return True
-unify p v@(V _)      = unify v p
-unify (C x) (C y) | x == y = return True
+unify p        v@(V _)             = unify v p
+unify (C x)    (C y)     | x == y  = return True
 unify (S n t1) (S n' t2) | n == n' = go t1 t2 where
     go []     []     = return True
     go (x:xs) (y:ys) = (&&) `fmap` unify x y `ap` go xs ys
     go _      _      = return False
-unify _        _     = return False
+unify _        _                   = return False
 
-prolog :: Term -> Term -> Maybe [(Char,Term)]
-prolog t1 t2 = let (a,s) = runState (unify t1 t2) empty in if a then Just (toList s) else Nothing
+(=?) :: Term -> Term -> Maybe [(Char,Term)]
+t1 =? t2 = let (a,s) = runState (unify t1 t2) empty in if a then Just (toList s) else Nothing
