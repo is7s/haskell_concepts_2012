ce1f3992af534d017f21cb8cbd598420c163d79f
Hussain Sobhy <hussain.sob7y@gmail.com>
2012-06-01 01:29:08 +0200

    Changed zipWithM' to zipWithM'_

--- main.hs
+++ main.hs
@@ -9,13 +9,13 @@ unify (V x)    (V y)     | x == y  = get
 unify (V c)  x           = get >>= \env -> when (notMember c env) (modify (insert c x)) >> get >>= unify x . (!c)
 unify p        v@(V _)   = unify v p
 unify (C x)    (C y)     | x == y  = get
-unify (S n t1) (S n' t2) | n == n' = zipWithM' unify t1 t2 >> get
+unify (S n t1) (S n' t2) | n == n' = zipWithM'_ unify t1 t2 >> get
 unify _        _         = lift Nothing
 
-zipWithM' :: (MonadPlus m, Functor m) => (a -> b -> m c) -> [a] -> [b] -> m [c]
-zipWithM' _ []     []     = return []
-zipWithM' f (x:xs) (y:ys) = (:) `fmap` f x y `ap` zipWithM' f xs ys
-zipWithM' _ _      _      = mzero
+zipWithM'_ :: (MonadPlus m) => (a -> b -> m c) -> [a] -> [b] -> m ()
+zipWithM'_ _ []     []     = return ()
+zipWithM'_ f (x:xs) (y:ys) = f x y >> zipWithM'_ f xs ys
+zipWithM'_ _ _      _      = mzero
 
 (=?) :: Term -> Term -> Maybe (Map Char Term)
 t1 =? t2 = evalStateT (unify t1 t2) empty
