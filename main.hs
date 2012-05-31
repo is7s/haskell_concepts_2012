4d1410d5845cac1fc1a2c6c6eee179f27061303f
Hussain Sobhy <hussain.sob7y@gmail.com>
2012-06-01 00:53:56 +0200

    Better version with zipWithM, handled complex recursive cases

--- main.hs
+++ main.hs
@@ -1,18 +1,21 @@
-import Control.Monad.State
-import Data.Map (Map,(!),member,insert,empty,toList)
+import Control.Monad.State.Strict
+import Data.Map (Map,(!),insert,empty,notMember)
+import Debug.Trace
 
 data Term = C String | V Char | S String [Term] deriving (Show,Eq)
 
-unify :: Term -> Term -> State (Map Char Term) Bool
-unify (V c)    x                   = get >>= \env -> if member c env 
-    then unify (env!c) x else modify (insert c x) >> return True
-unify p        v@(V _)             = unify v p
-unify (C x)    (C y)     | x == y  = return True
-unify (S n t1) (S n' t2) | n == n' = go t1 t2 where
-    go []     []     = return True
-    go (x:xs) (y:ys) = (&&) `fmap` unify x y `ap` go xs ys
-    go _      _      = return False
-unify _        _                   = return False
+unify :: Term -> Term -> StateT (Map Char Term) Maybe (Map Char Term)
+unify (V x)    (V y)     | x == y  = get
+unify (V c)  x           = get >>= \env -> when (notMember c env) (modify (insert c x)) >> get >>= unify x . (!c)
+unify p        v@(V _)   = unify v p
+unify (C x)    (C y)     | x == y  = get
+unify (S n t1) (S n' t2) | n == n' = zipWithM' unify t1 t2 >> get
+unify _        _         = lift Nothing
 
-(=?) :: Term -> Term -> Maybe [(Char,Term)]
-t1 =? t2 = let (a,s) = runState (unify t1 t2) empty in if a then Just (toList s) else Nothing
+zipWithM' :: (MonadPlus m, Functor m) => (a -> b -> m c) -> [a] -> [b] -> m [c]
+zipWithM' _ []     []     = return []
+zipWithM' f (x:xs) (y:ys) = (:) `fmap` f x y `ap` zipWithM' f xs ys
+zipWithM' _ _      _      = mzero
+
+(=?) :: Term -> Term -> Maybe (Map Char Term)
+t1 =? t2 = evalStateT (unify t1 t2) empty
