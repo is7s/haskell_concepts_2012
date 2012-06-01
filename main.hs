b9056a8b53fb4d355f475ce92d0ad71a441c5d76
Hussain Sobhy <hussain.sob7y@gmail.com>
2012-06-01 13:11:45 +0200

    Changed the return type of the unify state transformer to () instead of s

--- main.hs
+++ main.hs
@@ -4,12 +4,12 @@ import Debug.Trace
 
 data Term = C String | V Char | S String [Term] deriving (Show,Eq)
 
-unify :: Term -> Term -> StateT (Map Char Term) Maybe (Map Char Term)
-unify (V x)    (V y)     | x == y  = get
+unify :: Term -> Term -> StateT (Map Char Term) Maybe ()
+unify (V x)    (V y)     | x == y  = return ()
 unify (V c)  x           = get >>= \env -> when (notMember c env) (modify (insert c x)) >> get >>= unify x . (!c)
 unify p        v@(V _)   = unify v p
-unify (C x)    (C y)     | x == y  = get
-unify (S n t1) (S n' t2) | n == n' = zipWithM'_ unify t1 t2 >> get
+unify (C x)    (C y)     | x == y  = return ()
+unify (S n t1) (S n' t2) | n == n' = zipWithM'_ unify t1 t2
 unify _        _         = lift Nothing
 
 zipWithM'_ :: (MonadPlus m) => (a -> b -> m c) -> [a] -> [b] -> m ()
@@ -18,4 +18,4 @@ zipWithM'_ f (x:xs) (y:ys) = f x y >> zipWithM'_ f xs ys
 zipWithM'_ _ _      _      = mzero
 
 (=?) :: Term -> Term -> Maybe (Map Char Term)
-t1 =? t2 = evalStateT (unify t1 t2) empty
+t1 =? t2 = execStateT (unify t1 t2) empty
